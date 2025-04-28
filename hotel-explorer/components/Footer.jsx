be95fe75d19d3033844d0caee1a919dc4bd19ed2
Marcelo <mvarg005@odu.edu>
2025-04-28 13:18:38 -0400

    feat: create footer component

---
+++ hotel-explorer/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: '1rem', borderTop: '1px solid #ccc', marginTop: '2rem', textAlign: 'center' }}>
+      <p>© 2025 Hotel Explorer. All rights reserved.</p>
+    </footer>
+  );
+}
