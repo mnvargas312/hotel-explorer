48d367664f88e8da7be4a53356c4b3461f49e402
Marcelo <mvarg005@odu.edu>
2025-04-28 10:16:06 -0400

    fix: dynamically load hotels data in HotelDetailPage to properly await params

--- hotel-explorer/app/hotels/[slug]/page.jsx
+++ hotel-explorer/app/hotels/[slug]/page.jsx
@@ -1,15 +1,21 @@
-import hotels from '../../../data/hotels.json';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-export default function HotelDetailPage({params}){
+export default async function HotelDetailPage({ params }) {
+  // Read hotels.json dynamically
+  const filePath = path.join(process.cwd(), 'data', 'hotels.json');
+  const jsonData = await fs.readFile(filePath, 'utf8');
+  const hotels = JSON.parse(jsonData);
+
   const hotel = hotels.find(h => h.slug === params.slug);
-  
+
   if (!hotel) return <div>Hotel not found</div>;
 
-  return(
+  return (
     <div>
       <h1>{hotel.name}</h1>
       <p>{hotel.description}</p>
-      <img src ={hotel.image} alt = {hotel.name} width = {400} />
+      <img src={`${hotel.image}`} alt={hotel.name} width={400} />
     </div>
   );
 }
